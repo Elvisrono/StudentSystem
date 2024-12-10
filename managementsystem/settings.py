c007d699691c86485bca6e634d8f571942ba7319
ELVIS RONO <Elvis>
2024-12-10 20:13:06 +0300

    update the admin page ui

--- managementsystem/settings.py
+++ managementsystem/settings.py
@@ -124,11 +124,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+
+# Directory to hold collected static files (for production)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional directories to look for static files
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+# Media files settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
